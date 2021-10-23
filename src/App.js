cd5e44d6cb4d3cf30a0257627d524b85078cd069
Hannah <hannahli6060@gmail.com>
2021-10-23 11:01:07 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import { React, useEffect, useState } from "react";
+import SearchBar from "./SearchBar";
 
 function App() {
+  // const url = "https://api.jikan.moe/v3https://api.jikan.moe/v3"
+  const [animeName, setAnimeName] = useState("Fate/Zero");
+  const [animes, setAnimes] = useState([]);
+  const [numOfAnimeToDisplay, setNumOfAnimeToDisplay] = useState(10);
+  const url = `https://api.jikan.moe/v3/search/anime?q="${animeName}&page=1`;
+  const [searchValue, setSearchValue] = useState("");
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch(url);
+        const data = await response.json();
+        const results = data.results;
+        setAnimes(results);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchData();
+  }, [animeName]);
+
+  const handleOnSearch = (event) => {
+    event.preventDefault();
+    if(searchValue!==""){
+      setAnimeName(searchValue);
+    }
+    else{
+      return null
+    }
+  };
+
+  const handleOnChange = (event) => {
+    setSearchValue(event.target.value);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="animeSearch">
+        <SearchBar
+          handleOnSearch={(event) => handleOnSearch(event)}
+          searchValue={searchValue}
+          handleOnChange={handleOnChange}
+        />
+        <div className="animeList">
+          { animes!==undefined && animes.length > 0 ? animes
+            .filter((element, index) => index < numOfAnimeToDisplay)
+            .map((anime, index) => {
+              const { score, image_url, title } = anime;
+              return (
+                <div className="anime" key={index}>
+                  <img src={image_url} className="animeImage"></img>
+                  <div className="animeDesc">
+                    <p className="title">{title}</p>
+                    <p className="rating">{score}</p>
+                  </div>
+                </div>
+              );
+            }): (
+              <div>No Search Results for"{searchValue}"</div>
+            )}
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/SearchBar.js
@@ -0,0 +1,33 @@
+import { React } from "react";
+import { AiOutlineSearch } from "react-icons/ai";
+
+const SearchBar = ({ handleOnSearch, searchValue, handleOnChange }) => {
+  return (
+    <div className="search-container">
+      <form
+        id="search-bar"
+        onSubmit={(event) => {
+          handleOnSearch(event, searchValue);
+        }}
+      >
+        <input
+          className="search search-input"
+          onChange={handleOnChange}
+          placeholder="Search Anime"
+        ></input>
+        <button
+          className="search btn"
+          type="submit"
+          form="search"
+          onClick={(event) => {
+            handleOnSearch(event, searchValue);
+          }}
+        >
+          <AiOutlineSearch className="search-icon" />
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default SearchBar;
